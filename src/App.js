a94fabfc1829e6ca14cc98e081dce876eedb83fe
saahil-12 <msaahilk12@gmail.com>
2023-11-22 16:58:07 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoList from './todo';
+
+
+function App() {
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <TodoList/>
+    </div>
+    
+  );
+}
+
+export default App;
